e2bb3451faa39daf7db8bc251a8af92347e96e74
Solo4 <akasha02@mail.ru>
2025-10-06 22:40:48 +0300

    upd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// тут какой-то код
+// это класс Мэйн
 public class Main {
     // это функция мэйн
     public static void main(String[] args) {
@@ -13,5 +13,12 @@ public class Main {
         System.out.println("1. Hello world");
         System.out.print("2. Hello world");
         System.out.println("3. Hello world");
+
+        System.out.println("-------------------------------");
+
+        // Текст и числа
+        System.out.println("This is the TEXT!"); // Это текст
+        System.out.println(1 + 1); // 2
+        System.out.println(1.5 + 1.5); // 3
     }
 }
