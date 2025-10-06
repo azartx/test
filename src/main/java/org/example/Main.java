092cff846cd00755cb95205609b1f5adee2d57b2
Solo4 <akasha02@mail.ru>
2025-10-06 22:32:02 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+// тут какой-то код
+public class Main {
+    // это функция мэйн
+    public static void main(String[] args) {
+        //*
+        // 1. Hello world
+        // ___
+        // 2. Hello world3. Hello world
+        // ___
+        // */
+        System.out.println("1. Hello world");
+        System.out.print("2. Hello world");
+        System.out.println("3. Hello world");
+    }
+}
